6279352ac4debb60a8c1f5ca4b4d537f485880b9
AdamZWinter <adam@bootstrapmusic.com>
2023-04-26 05:03:46 +0000

    cleaning up

--- app.py
+++ app.py
@@ -23,16 +23,7 @@ import __main__
 def is_cat(x): return x[0].isupper() 
 __main__.is_cat = is_cat
 
-# url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/3-Colored_Norwegian_Forest_Cat.jpg/800px-3-Colored_Norwegian_Forest_Cat.jpg'
-# dest = 'downloadedCat01.jpg'
-# download_url(url, dest, show_progress=False)
-# im = Image.open(dest)
-# im.to_thumb(256,256)
-# im.save("thumb"+ dest, "JPEG")
 learn = load_learner('model.pkl')
-# is_cat,_,probs = learn.predict(dest)
-# obj["isCat"] = is_cat
-
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
@@ -43,18 +34,26 @@ def hello():
         imgDir = "images/"
         filename = f"downloadedCat{randomInt}.jpg"
         dest = imgDir+filename
+        destNew = imgDir+"2"+filename
         download_url(url, dest, show_progress=False)
         #time.sleep(1)
-        im = Image.open(dest)
-        im.to_thumb(256,256)
-        destNew = imgDir+"2"+filename
-        im.save(destNew, "JPEG")
+        try:
+            im = Image.open(dest)
+            im.to_thumb(256,256)
+            im.save(destNew, "JPEG")
+        except:
+            obj["error"] = True
+            obj["message"] = "This picture does not work.  Try a different one."
+            return obj
+
         os.remove(dest)
         try:
             is_cat,_,probs = learn.predict(destNew)
         except:
             obj["error"] = True
             obj["message"] = "This picture does not work.  Try a different one."
+            return obj
+        
         obj["isCat"] = is_cat
         return obj
 
@@ -62,13 +61,3 @@ def hello():
     obj["error"] = True
     obj["message"] = 'isCat requires POST method.  You have made GET request'
     return obj
-#    return f"Is a cat?: {is_cat}. \n"
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-# print(f"Is a cat?: {is_cat}.")
-# print(f"Probability it's a cat: {probs[0]}")
-
-# print("done")
